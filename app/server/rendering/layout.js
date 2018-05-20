2a57b4d4d241196cb89f301bdc32303be0fa8ff3
Bruno Scopelliti <bru.scopelliti@gmail.com>
2018-05-20 06:20:43 +0200

    Add CSS

--- app/server/rendering/layout.js
+++ app/server/rendering/layout.js
@@ -6,6 +6,8 @@ export default (html, meta, preloadedState) => `
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>${meta.title}</title>
   <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700&amp;subset=latin-ext" rel="stylesheet">
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+  <link rel="stylesheet" href="/css/index.css"/>
 </head>
 <body>
   <div id="root">${html}</div>
