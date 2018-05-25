088042a10793085b2342e916b3ef88d3022a557a
Bruno Scopelliti <bru.scopelliti@gmail.com>
2018-05-25 07:08:48 +0200

    Added @jscore/envar

--- app/server/bootstrap/tasks/app-settings.js
+++ app/server/bootstrap/tasks/app-settings.js
@@ -1,3 +1,5 @@
+const normalize = require("@jscore/envar");
+
 class AppSettingsTask {
   constructor () {
     this.name = "APPSETTINGS";
@@ -6,7 +8,7 @@ class AppSettingsTask {
   run (app) {
     app.set("config",
       process.env.NODE_ENV === "production"
-        ? process.env
+        ? normalize(process.env)
         : Object.assign({}, require("server/env/index.json"))
     );
   }
